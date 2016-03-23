b7f7a230e2405e82283e81a3692dfda1f8da141b
Suota <slota.olga@gmail.com>
2016-03-23 23:57:01 +0100

    LU check method added

--- lab2/LU.java
+++ lab2/LU.java
@@ -1,41 +1,28 @@
 package lab2;
 
-// LU Decomposition based on Gaussian Method with usage of partial pivoting 
-
 import java.util.Random;
 
 import org.apache.commons.math3.linear.Array2DRowRealMatrix;
 import org.apache.commons.math3.linear.LUDecomposition;
 import org.apache.commons.math3.linear.RealMatrix;
 
-public class LU {
+public class LU2 {
 
-		double[] gaussJordan(double A [][], double b[], int N,double[][] L){
-			double x[] = new double[N];  // array of unknowns x0, x1, x2, x3,..,xN
-			int ind[] = new int[N];  // array with indexes of x, to operate on it before we find actual values of x
-			 
-			for (int i=0; i<N ; i++){
-				ind[i]=i;
-		}
+		void gaussJordan(double A [][], int N,double[][] L){ 
+		
 			for(int col=0 ; col<N ; col++){
-				this.pivoting(col, N, A, b, ind);
+				this.pivoting(col, N, A);
 				for(int row=col+1; row<N ; row++ ){
-					this.calculateRow(row, col, A, b , N,L); 
+					this.calculateRow(row, col, A,  N,L); 
 				}
 			}
-			for ( int i=0 ; i< N ; i++){
-				x[i] = b[i]/A[i][i];
-			}
-			
-			return x;
 		}
 		
-		void calculateRow(int row , int col, double A[][] ,double b[], int N, double[][] L){
+		void calculateRow(int row , int col, double A[][], int N, double[][] L){
 			double multi =  A[row][col]/A[col][col];		
 			for ( int i = 0 ; i<N; i++){
 				A[row][i] -= multi * A[col][i];	
 				}
-			b[row] -= multi * b[col];
 			L[row][col]=multi;
 		}
 		
@@ -51,48 +38,27 @@ public class LU {
 			}
 		}
 		
-		void pivoting(int start, int N , double A[][], double b[], int ind[]){
+		void pivoting(int start, int N , double A[][]){
 			int x =0;
-			int y =0 ;
 			double max =0;
 			int j =start ;	
 			for ( int i = start ; i<N ; i++){
 				if (A[i][j]>Math.abs(max)){
 					max = A[i][j];
 					x= i;
-					y =j;
 				}	
 			}
 			this.swapRows(A[start], A[x],N);
-			this.swap(b[start], b[x]);
-			
 		}
 		
-		void showResults(int n, double A [][] , double b[], double [][] L ){
+		void showResults(int n, double A [][] , double [][] L ){
 			
-			LU z = new LU();	
-					
-				double res[] = new double[n];
-				res = z.gaussJordan(A, b, n,L);
-				
-				System.out.println("U MATRIX AFTER CALCULATION:\n");
-				for(int i =0 ; i<n ; i++){
-					for(int j =0 ; j<n ; j++)
-						System.out.printf("%16f ",A[i][j]);
-					System.out.println();
-				}
-				System.out.println("L MATRIX AFTER CALCULATION:\n");
-				for(int i =0 ; i<n ; i++){
-					for(int j =0 ; j<n ; j++)
-						System.out.printf("%16f ",L[i][j]);
-					System.out.println();
-				}
-				System.out.println("Solution:");
-				for(int i =0 ; i<n ; i++){
-					System.out.print(res[i]+" ");	
-				}
-				
-				System.out.println("\n");
+			LU2 z = new LU2();	
+			z.gaussJordan(A, n,L);
+			System.out.println("My solution");
+			z.printMatrix(A,n);
+			System.out.println();
+			z.printMatrix(L,n);
 		}
 		
 	double[][] prepareL(int n){
@@ -103,8 +69,7 @@ public class LU {
 					L[i][i]=1;
 				else
 				L[i][j] = 0;
-			}
-							
+			}					
 		return L;
 	}
 	
@@ -115,29 +80,57 @@ public class LU {
 			System.out.println();
 		}
 	}
+	public boolean check(int SIZE, double OriginalMatrix[][], double Matrix[][] , double[][] L) {
+		double CheckMatrix[][] = new double[SIZE][SIZE];
+		
+		for(int i = 0; i < SIZE; i++) {
+			for(int j = 0; j < SIZE; j++) {
+				double result = 0.0;
+				
+				for(int s = 0; s < SIZE; s++) {
+					result = result + (L[i][s] * Matrix[s][j]);
+				}
+				CheckMatrix[i][j] = result;			
+			}
+		}
+		boolean isTheSame = true;
+		
+		for(int i = 0; i < SIZE; i++) {
+			for(int j = 0; j < SIZE; j++) {
+				if(Math.abs(OriginalMatrix[i][j] - CheckMatrix[i][j]) > 1) {
+					return false;
+				}
+			}
+		}
+		return isTheSame;
+	}
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
 		Random r = new Random();
 		int n= 10;
 		double A[][] = new double [n][n];
-		double b[] = new double [n];
-		
-		
 		
 		for (int i=0; i< n ; i++)
 			for(int j =0 ; j< n ; j++){
 				A[i][j] = r.nextDouble()*100 * r.nextInt(100)-50;
-				b[j]= r.nextDouble()*2;
 					}
 		RealMatrix m = new Array2DRowRealMatrix(A);
-
-	LU lu = new LU();
+		
+		double Original[][] = new double[n][n];
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < n; j++) {
+				Original[i][j] = A[i][j];
+			}
+		}
+	LU2 lu = new LU2();
 	double[][]L = lu.prepareL(n);
-	lu.showResults(n, A, b,L);
+	lu.showResults(n, A,L);
+	if(lu.check(n, Original, A, L))
+		System.out.println("Factorization correct");
+	else 
+		System.out.println("Factorization incorrect!\n");
 	
-
 	//****************************************
 	
 	LUDecomposition LU = new LUDecomposition(m);
@@ -146,7 +139,5 @@ public class LU {
 	lu.printMatrix(LU.getU().getData(),n);	
 	System.out.println("\nL");
 	lu.printMatrix(LU.getL().getData(),n);
-	
 	}
-
 }
